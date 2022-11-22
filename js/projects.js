164cfcf58d2b23329cefe7f1231d890837167a4a
NataliaChueco <natchueco@gmail.com>
2022-11-22 22:06:30 +0100

    Update pokemons page "routing" and current date

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,5 @@
-
-  fetch(`https://pokeapi.co/api/v2/pokemon`)
+const urlParams = new URLSearchParams(window.location.search);
+fetch(`https://pokeapi.co/api/v2/pokemon`)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`);
@@ -13,7 +13,7 @@
     let pokeAbilities = "";
     let weight = "";
 
-    fetch(data.results[3].url)
+    fetch(data.results[urlParams.get('pokemon')*3].url)
       .then((response) => {
         if (!response.ok) {
           return "";
@@ -43,16 +43,16 @@
       })
       .then(() => {
         //Setting the variables with the needed info
-        let title = `Choose ${data.results[3].name}`;
+        let title = `Choose ${data.results[urlParams.get('pokemon')*3].name}`;
 
         let descr =
           "Starter pokémon " +
-          data.results[3].name +
+          data.results[urlParams.get('pokemon')*3].name +
           " is type " +
           poketype;
           let longdescr =
           "This little starter pokémon called " +
-          data.results[3].name +
+          data.results[urlParams.get('pokemon')*3].name +
           " is type " +
           poketype +
           ". As in every Pokémon story you have to choose a starting Pokémon, surely no one decides to take the plant type, you will surely choose the fire type because you are the most basic and rat child there is. But it doesn't really matter, because you'll end up looking at which one is the prettiest or the coolest and at most giving them funny names like Eustace or if you're even more geeky, Dracarys, I don't know. Anyway, in case you are really interested, their abilities are these: " +
@@ -63,7 +63,7 @@
 
 
         //Substituting the elements from the DOM
-        /* document.getElementById("image").style.backgroundImage.url = img; */
+        document.getElementById("image").style.backgroundImage=`url(/ironhack-mid-term-project/assets/projects-section/${data.results[urlParams.get('pokemon')*3].name}.jpg)`; 
 
         let HTMLTitle = document.getElementById(`title`);
         HTMLTitle.innerHTML = title;
@@ -71,8 +71,16 @@
         let HTMLDescr = document.getElementById(`descr`);
         HTMLDescr.innerHTML = descr;
 
+        var date = new Date();
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+	      var current_date = monthNames[(date.getMonth())]+" "+date.getDate()+", "+ date.getFullYear();
+	      document.getElementById("currentDate").innerHTML = "Completed on " + current_date;
+
         let HTMLLongDescr = document.getElementById(`longdescr`);
         HTMLLongDescr.innerHTML = longdescr;
+
       });
   })
   .catch((error) => console.log(error));
