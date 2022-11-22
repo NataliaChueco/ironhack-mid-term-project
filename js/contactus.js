be3394f33cc8fefbd761ab939139513b78f6234a
NataliaChueco <natchueco@gmail.com>
2022-11-22 20:51:05 +0100

    Email valid and empty fields

--- js/contactus.js
+++ js/contactus.js
@@ -1,20 +1,56 @@
 const submit = document.getElementById("submit");
+const closebtn = document.getElementsByClassName("closebtn");
 
-submit.addEventListener("click", validate);
 
-function validate(e) {
+submit.addEventListener("click", notifyFields);
+
+
+function notifyFields(e) {
   e.preventDefault();
 
   const fullNameField = document.getElementById("fname");
-  let valid = true;
+  const emailField = document.getElementById("email");
+  const phoneField = document.getElementById("phone");
 
-  if (!fullNameField.value) {
-    const alert = document.getElementById("alert");
-    
-    alert.classList.add("visible");
-    alert.setAttribute("aria-hidden", false);
-    alert.setAttribute("aria-invalid", true);
+  if (!validateEmail(emailField)){
+    showErrorAlert("errorEmail");
+  }
+
+  if (!fullNameField.value || !emailField.value || !phoneField.value ) {
+    showErrorAlert("alert");
     
   }
-  return valid;
+  else{
+    showErrorAlert("submit");
+  }
+
+  const inputs = document.querySelectorAll('#fname, #email','#phone' );
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+
+
+function validateEmail(mail){ 
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+    {
+      return true;
+    }
+      console.log("You have entered an invalid email address!");
+      return false;
+}
+
+function showErrorAlert(id){
+  const alert = document.getElementById(id);
+  alert.style.display = "block";
+  alert.setAttribute("aria-hidden", false);
+  alert.setAttribute("aria-invalid", true);
+}
+
+function closeAlert(id){
+  const alert = document.getElementById(id);
+ alert.parentNode.style.display = "none";
+  
 }
